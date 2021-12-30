90ba50f6d55f7205b966b4e310fb1a0d65d5c655
Gadzhi Kharkharov <me@kkga.me>
2021-12-30 03:47:44 +0300

    upd

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 export default /** @type {import('astro').AstroUserConfig} */ ({
   // all options are optional; these values are the defaults
-  projectRoot: "./",
-  public: "./public/",
-  dist: "./dist/",
-  src: "./src/",
-  pages: "./src/pages/",
+  // projectRoot: "./",
+  // public: "./public/",
+  // dist: "./dist/",
+  // src: "./src/",
+  // pages: "./src/pages/",
   buildOptions: {
     site: "https://kkga.me",
     sitemap: true,
