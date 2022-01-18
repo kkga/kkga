24977379bea0240f924a5ec6c47848e0f9790fda
Gadzhi Kharkharov <kkga@users.noreply.github.com>
2022-01-18 23:47:40 +0300

    upd

--- src/components/Listing.jsx
+++ src/components/Listing.jsx
@@ -4,7 +4,7 @@ export default function Listing({ title, items }) {
   return (
     <div>
       <h2 className="mt-0 mb-4">{title}</h2>
-      <ul className="flex flex-col list-none gap-x-8 divide-y dark:divide-gray-500 sm:divide-y-0">
+      <ul className="flex flex-col list-none gap-x-8 divide-y dark:divide-gray-700 sm:divide-y-0">
         {items.map((item) => <ListItem key={item.url} {...item} />)}
       </ul>
     </div>
@@ -20,7 +20,7 @@ function ListItem({ title, url, description, date }) {
       >
         {title}
       </a>
-      <div className="hidden sm:block basis-16 flex-1 mt-1 border-t-2 h-1 border-dotted border-gray-400 dark:border-gray-900">
+      <div className="hidden sm:block basis-16 flex-1 mt-1 border-t-2 h-1 border-dotted border-gray-400 dark:border-gray-700">
       </div>
       {description &&
         (
