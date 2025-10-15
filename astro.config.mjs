22a91e06c4512a7809f6fc4bcda3ccbaea949c94
Gadzhi Kharkharov <me@kkga.me>
2025-10-15 22:26:09 +0300

    upd

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,57 +1,35 @@
 // @ts-check
-import { defineConfig, sharpImageService } from "astro/config";
+import { defineConfig, fontProviders, sharpImageService } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://kkga.me",
-  integrations: [mdx()],
-  vite: {
-    plugins: [tailwindcss()],
-  },
-  image: {
-    service: sharpImageService({ limitInputPixels: false }),
-    responsiveStyles: true,
-    layout: "constrained",
-  },
-  markdown: {
-    shikiConfig: {
-      theme: "css-variables",
-    },
-  },
-  experimental: {
-    fonts: [
-      {
-        provider: "local",
-        name: "PP Nikkei Pacific",
-        cssVariable: "--font-pp-nikkei-pacific",
-        variants: [
-          {
-            weight: "400",
-            style: "normal",
-            src: [
-              "./src/fonts/PPNikkeiPacific-Regular.woff2",
-              "./src/fonts/PPNikkeiPacific-Regular.woff",
-            ],
-          },
-        ],
-      },
-      {
-        provider: "local",
-        name: "Intel One Mono",
-        cssVariable: "--font-intel-one-mono",
-        variants: [
-          {
-            weight: "400",
-            style: "normal",
-            src: [
-              "./src/fonts/IntelOneMono-Regular.woff2",
-              "./src/fonts/IntelOneMono-Regular.woff",
-            ],
-          },
-        ],
-      },
-    ],
-  },
+	site: "https://kkga.me",
+	integrations: [mdx()],
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	image: {
+		service: sharpImageService({ limitInputPixels: false }),
+		responsiveStyles: true,
+		layout: "constrained",
+	},
+	markdown: {
+		shikiConfig: {
+			theme: "css-variables",
+		},
+	},
+	experimental: {
+		fonts: [
+			{
+				provider: fontProviders.google(),
+				name: "Fragment Mono",
+				cssVariable: "--font-fragment-mono",
+        styles: ["normal"],
+        fallbacks: ["monospace"],
+        
+			},
+		],
+	},
 });
