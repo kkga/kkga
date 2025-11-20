f42228348c20e7ed5cf878a3513d8587bef9c98f
Gadzhi Kharkharov <me@kkga.me>
2025-11-20 11:07:03 +0200

    upd

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import eslintPluginAstro from "eslint-plugin-astro";
 export default [
   // add more generic rule sets here, such as:
   // js.configs.recommended,
-  ...eslintPluginAstro.configs.recommended,
+  ...eslintPluginAstro.configs["jsx-a11y-recommended"],
   {
     rules: {
       // override/add rules settings here, such as:
