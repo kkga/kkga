d73f33fc1a23bc0db5532d4b6541f1ee6bf3f5a9
Gadzhi Kharkharov <me@kkga.me>
2026-01-24 14:40:08 +0200

    optimize assets, improve seo/a11y/linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,19 @@
 import eslintPluginAstro from "eslint-plugin-astro";
+import typescriptEslintParser from "@typescript-eslint/parser";
+
 export default [
-  // add more generic rule sets here, such as:
-  // js.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
   ...eslintPluginAstro.configs["jsx-a11y-recommended"],
   {
+    files: ["**/*.astro"],
+    languageOptions: {
+      parserOptions: {
+        parser: typescriptEslintParser,
+        extraFileExtensions: [".astro"],
+      },
+    },
     rules: {
-      // override/add rules settings here, such as:
-      // "astro/no-set-html-directive": "error"
+      "astro/jsx-a11y/media-has-caption": "off",
     },
   },
 ];
